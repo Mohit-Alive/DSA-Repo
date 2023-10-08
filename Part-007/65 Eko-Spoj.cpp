059a5144eae69572540bcae4ce7fe60d5efee8bc
Mohit-Alive <mohitkh22@gmail.com>
2023-10-08 11:03:50 +0530

    8th commit

---
+++ Part-007/65 Eko-Spoj.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+// EKO SPOJ
+
+bool isPossibleSol(vector<long long int>trees, long long int m,long long int mid){
+    long long int woodCollected=0;
+    for(long long int i=0; i<trees.size();i++){
+        if(trees[i]>mid){
+            woodCollected+=trees[i]-mid;
+        }
+
+    }   
+    return woodCollected>=m;
+}
+
+long long int maxSawBladeHeight(vector<long long int>trees, long long int m){
+    long long int start=0,end,ans=-1;
+    end=*max_element(trees.begin(),trees.end());
+
+    while(start<=end){
+        long long int mid= start+(end-start)/2;
+        if(isPossibleSol(trees,m,mid)){
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
+
+}
+
+int main(){
+    long long int n ,m;
+    cin>>n>>m;
+    vector<long long int> trees;
+    while(n--){
+        long long int height;
+        cin>>height;
+        trees.push_back(height);
+
+    }
+    cout<<maxSawBladeHeight(trees,m)<<endl;
+    
+      
+    return 0;
+}
+//n=4 ,m=7
+// 20 15 10 17
