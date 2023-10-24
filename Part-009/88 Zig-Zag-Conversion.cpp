bacd9a9131aa2540a55d7461bfafa43f3914534d
Mohit-Alive <mohitkh22@gmail.com>
2023-10-24 18:01:12 +0530

    10th commit

---
+++ Part-009/88 Zig-Zag-Conversion.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Zig Zag Conversion
+
+string convert(string s, int numRows){
+    if(numRows==1) return s;
+
+    vector<string>zigzag(numRows);
+
+    int i=0,row=0;
+    bool direction =1;   // 1--> T-->B
+
+    while(true){
+        if(direction){
+            while(row<numRows && i<s.size()){
+                zigzag[row++].push_back(s[i++]);
+            }
+            row=numRows-2;
+        }
+        else{
+            while(row>=0 && i<s.size()){
+                zigzag[row--].push_back(s[i++]); 
+            }
+            row= 1;
+        }
+        if(i>=s.size()) break;
+        direction =!direction;
+    }
+    string ans="";
+    for(int i=0;i<zigzag.size();i++){
+        ans+=zigzag[i];
+        cout<<zigzag[i]<<endl;
+    }
+    return ans;
+}
+
+int main(){
+    string s="ABCDEFG";
+    int num = 3;
+    cout<<convert(s,num)<<endl;
+    
+    return 0;
+}
