3cd53ffbd4a354442a59850a7ad0451ffe46d5b0
mohit-alive <mohitkh22@gmail.com>
2024-02-23 17:10:27 +0530

    Addition

---
+++ Part-010/100-Refrence Variable-Another.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//Important
+
+int* solve(){
+    int a= 5;
+    int *ans = &a;
+    return ans;
+}
+
+int main(){
+    
+    return 0;
+}
